ae03f5605f90c13b0fa77687ca14939be3eb6774
Devesh2102 <deveshkumar2102@gmail.com>
2025-05-20 05:59:31 +0000

    loader added and deleting the posts

--- src/api/PostApi.js
+++ src/api/PostApi.js
@@ -7,3 +7,8 @@ const api = axios.create({
 export const getPost = () => {
     return api.get('/posts')
 }
+
+
+export const deletePost = (id) => {
+    return api.delete(`/posts/${id}`)
+}
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,38 +1,74 @@
 import {useEffect} from "react";
 import {getPost } from "../api/PostApi";
+import { deletePost } from "../api/PostApi";
 import {useState} from "react";
+import "../App.css" 
 
 const Posts = () => {
 
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
 
   const getPostData = async () => {
-    const res = await getPost();
-    setData(res.data);
-    console.log(res.data)
+    setLoading(true); 
+    try {
+      const res = await getPost();
+      setData(res.data);
+    } catch (err) {
+      console.error("Failed to fetch posts:", err);
+    }
+    setLoading(false);
+  }
+
+  const handlerDeletePost = async (id)=>{
+    setLoading(true);
+    try{
+      const res = await deletePost(id);
+      if(res.status === 200){
+        const newUpdatedData = data.filter((currentPost)=>{
+          return currentPost.id !== id;
+        })
+        setData(newUpdatedData);
+      }
+    }
+    catch (error){
+      console.log("failed to delete the post", error);
+    }
+    setLoading(false);
   }
 
   useEffect(() => {
     getPostData();
   }, []); 
 
-
     return <section className = "section-post">
-      <ul>
-        {
-          data.map((currentElement)=> {
-            const {id, title, body} = currentElement;
-            return(
-              <li key={id}>
-                <p>{id}</p>
-                <p>{title}</p>
-                <p>{body}</p>
-              </li>
-            )
-
-          })
-        }
-      </ul>
+    {
+      loading ? (
+      <div class="loader">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+    </div>
+      ) : 
+      data.map((currentElement) => {
+        const {id, title, body} = currentElement;
+        return(
+          <div key={id} className="post-card">
+            <h3>{id}. {title}</h3>
+            <p>{body}</p>
+            <div className="post-actions">
+              <button className="btn-edit">Edit</button>
+              <button className="btn-delete" onClick={()=> handlerDeletePost(id)}>Delete</button>
+            </div>
+          </div>
+        )
+      })
+    }
     </section>
 }
 
